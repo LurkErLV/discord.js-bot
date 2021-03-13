00e37be7224ec80f1ad39d4ce9bea36e080d322f
LurkErLV <62105039+LurkErLV@users.noreply.github.com>
2021-03-13 02:13:03 +0200

    Update pinfo.js

--- commands/pinfo.js
+++ commands/pinfo.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports = {
     name: "pinfo",
-    description: "Info about person",
+    description: "Person information",
     async execute(message, args) {
         const { guild, channel } = message;
 
